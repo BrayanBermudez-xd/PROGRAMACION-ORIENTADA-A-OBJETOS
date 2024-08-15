4c50db04513b039730f7d7e64b055072be9a3fd4
BrayanBermudez-xd <brayanelpro56@gmail.com>
2024-08-14 21:49:30 -0600

    primero jsjsjs

--- README.md
+++ README.md
@@ -5,7 +5,10 @@
 - Email: brayanelpro56@gmail.com
 
 ## Actividad 1
-
+- p01_HolaMundo
+- p02_AreaCirculo
+- p03_AreaTriangulo
+- P04_PagaTrabajador
 
 ## Actividad 2
 
---
+++ p01_HolaMundo.java
@@ -0,0 +1,15 @@
+/**
+* p01_HolaMundo 
+*/
+public class p01_HolaMundo {
+    public static void main(String[] args) {
+        String amigo = "Kakaroto";
+        String hermano = "Mclovin";
+        String mensaje = String.format("aaaaaaaaaaaa es el negro" ,amigo);
+
+        System.out.println("hola papus");
+        System.out.println("Hola Mundo en el lenguaje java");
+        System.out.println("\nHola amigo " + amigo + " bienvenido a java");
+    }
+}
+

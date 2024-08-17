748aebef7005c2a82d904d7c514bfec755079bf5
BrayanBermudez-xd <brayanelpro56@gmail.com>
2024-08-16 23:28:13 -0600

    Actividad 1

--- p01_HolaMundo.java
+++ p01_HolaMundo.java
@@ -5,7 +5,7 @@ public class p01_HolaMundo {
     public static void main(String[] args) {
         String amigo = "Kakaroto";
         String hermano = "Mclovin";
-        String mensaje = String.format("aaaaaaaaaaaa es el negro" ,amigo);
+        String mensaje = String.format("aaaaaaaaaaaa es el negroooo" ,amigo);
 
         System.out.println("hola papus");
         System.out.println("Hola Mundo en el lenguaje java");

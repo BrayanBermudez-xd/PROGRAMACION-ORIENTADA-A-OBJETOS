ddbb0a813a9a798458462d58f3b26dab7ffc0981
BrayanBermudez-xd <brayanelpro56@gmail.com>
2024-09-10 15:52:39 -0600

    Examen primer parcial

--- pe01_PrimerExamenParcial.java
+++ pe01_PrimerExamenParcial.java
@@ -8,11 +8,12 @@ import java.util.Scanner;
 public class pe01_PrimerExamenParcial {
     public static void main(String[] args) {
         String nombre;
-        int edad, cont, contH, contM, contA, contD, contT, contR;
+        int edad, cont, contH, contM, a , z, y, fin;
+        int contA, contD, contT, contR;
         int totH, totM, totA, totD, totT, totR,totp, prom;
         char sexo, participante ,resp ;
         cont = 1;
-        edad=contH= contM= contA= contD= contT= contR=0;
+        edad= contH= contM= contA= contD= contT= contR= totH= totM= totA= totD= totT= totR=totp= prom= 0;
         Scanner obj = new Scanner(System.in);
         System.out.print("\033[H\033[2J"); System.out.flush();
 
@@ -36,10 +37,10 @@ public class pe01_PrimerExamenParcial {
 
        switch (sexo) {
         case 'H': contH = sexo;  
-                totH = contH +=1;        
+                totH +=1;        
             break;
        case 'M' : contM = sexo;
-                totM = contM +=1;
+                totM +=1;
         default:
             break;
        }
@@ -47,23 +48,23 @@ public class pe01_PrimerExamenParcial {
         switch (participante) {
             case 'D':System.out.printf("BIENVENIDO " + participante + "" + nombre + " ahora formas parte de nuestras lienas" );
                     contD = participante;
-                    totD = contD +=1;
+                    totD +=1;
             break;
             case 'T': System.out.printf("BIENVENIDO " + participante + "" + nombre + " ahora formas parte de nuestras lienas" );
                     contT = participante; 
-                    totT = contT +=1;
+                    totT +=1;
             break;
             case'A': if (edad <= 23) {
                 System.out.printf("BIENVENIDO "+ participante + "" + nombre + " ahora formas parte de nuestras lienas" );
                 contA = participante; 
-                totA = contA +=1;
+                totA +=1;
                 break;
 
             }
                 else{
                     System.out.println("lo lamento solo admitimos a mayores de 23");
                     contR = participante;
-                    totR = contR +=1;
+                    totR +=1;
                     
                 
                 
@@ -83,19 +84,39 @@ public class pe01_PrimerExamenParcial {
     System.out.println("inscripciones realizadas " + (cont - 1));
 
     totp = totA + totD + totT;
-    prom = participante / edad;
-    System.out.printf("Total, de Alumnos                              : %d ",totA );
-    System.out.printf("Total, de Docentes                             : %d ",totD );
-    System.out.printf("Total, de Trabajadores                         : %d ",totT );
-    System.out.printf("Total, de Hombres en general                   : %d ",totH );
-    System.out.printf("Total, de Mujeres en general                   : %d ",totM );
-    System.out.printf("Total, de todos los participantes              : %d ",totp);
-    System.out.printf("Promedio de la edad de todos los participantes : %d ",prom );
-    System.out.printf("TTotal, de rechazados                          : %d ",totR );
+    prom = edad+edad/participante  ;
+    a = totA * 40;
+    z = totD * 60;
+    y = totT * 80;
+    fin = a+z+y;
 
+    System.out.printf("\nTotal, de Alumnos                              : %d ",totA );
+    System.out.printf("\nTotal, de Docentes                             : %d ",totD );
+    System.out.printf("\nTotal, de Trabajadores                         : %d ",totT );
+    System.out.printf("\nTotal, de Hombres en general                   : %d ",totH );
+    System.out.printf("\nTotal, de Mujeres en general                   : %d ",totM );
+    System.out.printf("\nTotal, de todos los participantes              : %d ",totp);
+    System.out.printf("\nPromedio de la edad de todos los participantes : %d ",prom );
+    System.out.printf("\nTotal, de rechazados                           : %d ",totR );
+
+
+    System.out.printf("\ntotal de dinero recaudado por Alumnos                     : %d    ",a);
+    System.out.printf("\ntotal de dinero recaudado por Docentes                    : %d    ",z);
+    System.out.printf("\ntotal de dinero recaudado por Trabajadores                : %d    ",y);
+    System.out.printf("\ntotal de dinero en general de todos los participantes     : %d    ",fin);
+
+    if (fin < 50) {
+        System.out.println("\nEl evento concluye con ganacias bajas");
+        
+    }if (fin > 50 && fin < 1000) {
+        System.out.println("\nEl evento concluye con ganacias moderadas");
+
+        
+    }if (fin > 1000) {
+        System.out.println("\nEl evento concluye con ganacias superadas");
+
+    }
+        
     
-
-
-
     }   
 }

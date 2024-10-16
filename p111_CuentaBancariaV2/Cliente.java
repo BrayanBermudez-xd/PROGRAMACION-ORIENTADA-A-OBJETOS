536cf9a40eddfb6e4d6094b9c2178f248a35c31e
BrayanBermudez-xd <brayanelpro56@gmail.com>
2024-10-15 23:57:32 -0600

    Actividad 19

--- p111_CuentaBancariaV2/Cliente.java
+++ p111_CuentaBancariaV2/Cliente.java
@@ -2,52 +2,41 @@ package p111_CuentaBancariaV2;
 
 import java.util.ArrayList;
 
+
 public class Cliente {
     private String nombre;
     private ArrayList<CuentaBancaria> cuentas;
-
-    public Cliente () {
+    
+    public Cliente(){
         cuentas = new ArrayList<>();
-
     }
-
-    public Cliente(String nombre, ArrayList<CuentaBancaria> cuentas) {
+    public Cliente(String nombre){
         this();
         this.nombre = nombre;
-
-        
     }
-
     public String getNombre() {
         return nombre;
     }
-
     public void setNombre(String nombre) {
         this.nombre = nombre;
+    }
+    
+    public void agregarCuenta(CuentaBancaria cuenta){
+        cuentas.add(cuenta);
     }
 
     public ArrayList<CuentaBancaria> getCuentas() {
         return cuentas;
     }
-    public double getTotal() {
-        double total = 0;
-        for (CuentaBancaria cuentaBancaria : cuentas) {
+    public double getTotal(){
+        double total=0;
+        for(CuentaBancaria cuenta : cuentas){
             total += cuenta.getSaldo();
         }
         return total;
     }
-
-    public void setCuentas(ArrayList<CuentaBancaria> cuentas) {
-        this.cuentas = cuentas;
-    }
-
     @Override
     public String toString() {
-        return "Cliente [nombre=" + nombre + ", cuentas=" + cuentas.size() + "]";
+        return "Cliente [nombre=" + nombre + ", cuentas=" + cuentas.size() + ", Total=" + this.getTotal() + "]";
     }
-    
-    
-
-
-    
 }
